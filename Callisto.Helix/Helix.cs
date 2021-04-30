0ee9928f8c80af6d9c9427da077e109e5ac47531
Shrey Kumar <skumar@callisto.enterprises>
2021-04-30 13:02:32 -0400

    Add project files.

---
+++ Callisto.Helix/Helix.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Callisto.Helix
+{
+    public record HelixResult
+    {
+        public bool Succeeded { get; set; }
+        public List<string> Errors { get; set; }
+
+        public static HelixResult Success => new() { Succeeded = true };
+        public static HelixResult Failed(List<string> errors) => new() { Errors = errors };
+        public static HelixResult Failed(string error) => new() { Errors = new List<string> { error } };
+    }
+
+    public record HelixValueResult<T> : HelixResult
+    {
+        public T Value { get; set; }
+        public static new HelixValueResult<T> Success(T value) => new() { Succeeded = true, Value = value };
+        public static new HelixValueResult<T> Failed(List<string> errors) => new() { Errors = errors };
+        public static new HelixValueResult<T> Failed(string error) => new() { Errors = new List<string> { error } };
+    }
+}
